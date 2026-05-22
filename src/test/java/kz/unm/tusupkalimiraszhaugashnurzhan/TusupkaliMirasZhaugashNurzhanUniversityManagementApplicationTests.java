e9d7ce3146130ba206858866e50ca7a5107418e0
Dezaster5 <Penxren20052110@gmail.com>
2026-05-22 11:51:35 +0500

    chore: configure PostgreSQL dependencies and application settings

--- src/test/java/kz/unm/tusupkalimiraszhaugashnurzhan/TusupkaliMirasZhaugashNurzhanUniversityManagementApplicationTests.java
+++ src/test/java/kz/unm/tusupkalimiraszhaugashnurzhan/TusupkaliMirasZhaugashNurzhanUniversityManagementApplicationTests.java
@@ -2,8 +2,10 @@ package kz.unm.tusupkalimiraszhaugashnurzhan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TusupkaliMirasZhaugashNurzhanUniversityManagementApplicationTests {
 
     @Test

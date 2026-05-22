bacfdcc1c25c582dc5b792f8631d9f6c0fd1b357
Dezaster5 <Penxren20052110@gmail.com>
2026-05-22 11:49:24 +0500

    chore: initialize Spring Boot project

---
+++ src/main/java/kz/unm/tusupkalimiraszhaugashnurzhan/TusupkaliMirasZhaugashNurzhanUniversityManagementApplication.java
@@ -0,0 +1,12 @@
+package kz.unm.tusupkalimiraszhaugashnurzhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusupkaliMirasZhaugashNurzhanUniversityManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TusupkaliMirasZhaugashNurzhanUniversityManagementApplication.class, args);
+    }
+}
---
+++ src/test/java/kz/unm/tusupkalimiraszhaugashnurzhan/TusupkaliMirasZhaugashNurzhanUniversityManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package kz.unm.tusupkalimiraszhaugashnurzhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusupkaliMirasZhaugashNurzhanUniversityManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
